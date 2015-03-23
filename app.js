1900be1fa23ca88e0bd9d4e1e3959e81a71b990e
Jeremy BLANC <jeremy.blanc@smile.fr>
2015-03-23 16:27:46 +0100

    Modification du code : prise en compte d'un son lors de 'lenvoie de message, sécurisation des champs (contre les champs vides)

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 var fs = require('fs');
 var ent = require('ent');
+var mongojs = require('mongojs');
 
 
 app.use(express.static(__dirname + '/css'));
@@ -13,7 +14,8 @@ var user_lists = [];
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 })
-.use(express.static(__dirname + '/css'));
+.use(express.static(__dirname + '/css'))
+.use(express.static(__dirname + '/public/sounds'));
 
 
 io.sockets.on('connection', function (socket, pseudo) {
@@ -23,17 +25,47 @@ io.sockets.on('connection', function (socket, pseudo) {
     // Dès qu'on nous donne un pseudo, on le stocke en variable de session
     socket.on('petit_nouveau', function(pseudo) {
 
+        // envoyer la liste des utilisateurs connectés
         socket.pseudo = ent.decode(pseudo);
         user_lists.push(pseudo);
         socket.broadcast.emit('petit_nouveau', socket.pseudo);
+
+        // envoyer les 10 derniers messages
+        var db = mongojs('localhost/chat', ['messages']);
+        db.messages.count(function(err, count) {
+
+            (db.messages.find().sort({_id:1}).skip(count-10)).forEach(function(err, docs) {
+
+                if (docs) {
+                    socket.emit('chat_to_client', {pseudo: docs.pseudo, message: docs.message, datetime: docs.date});
+                }
+            })
+        })
+
+
     });
 
     // Dès qu'on reçoit un "message" (clic sur le bouton), on le note dans la console
     socket.on('chat_server', function (message) {
 
+        var currentdate = new Date();
+        var datetime =  currentdate.getDate() + "/"
+            + (currentdate.getMonth()+1)  + "/"
+            + currentdate.getFullYear() + " @ "
+            + currentdate.getHours() + ":"
+            + currentdate.getMinutes() + ":"
+            + currentdate.getSeconds();
+
+
+        // envoyer le message à tout le monde
         var message = ent.decode(message);
-        socket.broadcast.emit('chat_to_client', {pseudo: socket.pseudo, message: message});
-        socket.emit('chat_to_client', {pseudo: socket.pseudo, message: message});
+        socket.broadcast.emit('chat_to_client', {pseudo: socket.pseudo, message: message, datetime: datetime});
+        socket.emit('chat_to_client', {pseudo: socket.pseudo, message: message, datetime: datetime});
+
+        // enregistrer le message dans MongoDB
+        var db = mongojs('localhost/chat', ['messages']);
+        db.messages.insert({date: datetime, pseudo: socket.pseudo, message: message});
+        db.close();
     });
 
     socket.on('user_leaving', function() {
